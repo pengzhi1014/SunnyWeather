e585dc5f7ccc580becc7f9ff353a86f291cc0124
pengzhi1014 <1433026519@qq.com>
2022-11-25 14:04:41 +0800

    新增切换城市和手动更新天气的功能

--- app/src/main/java/com/sunnyweather/android/ui/weather/WeatherActivity.kt
+++ app/src/main/java/com/sunnyweather/android/ui/weather/WeatherActivity.kt
@@ -1,16 +1,23 @@
 package com.sunnyweather.android.ui.weather
 
+import android.content.Context
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.ScriptGroup.Input
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.sunnyweather.android.R
+import com.sunnyweather.android.SunnyWeatherApplication
 import com.sunnyweather.android.logic.model.Weather
 import com.sunnyweather.android.logic.model.getSky
 import kotlinx.android.synthetic.main.activity_weather.*
@@ -28,38 +35,77 @@ class WeatherActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //融合状态栏
         val decorView = window.decorView
-        decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
         window.statusBarColor = Color.TRANSPARENT
 
         setContentView(R.layout.activity_weather)
 
-        if (viewModel.locationLng.isEmpty()){
+        if (viewModel.locationLng.isEmpty()) {
             viewModel.locationLng = intent.getStringExtra("location_lng") ?: ""
         }
 
-        if (viewModel.locationLat.isEmpty()){
+        if (viewModel.locationLat.isEmpty()) {
             viewModel.locationLat = intent.getStringExtra("location_lat") ?: ""
         }
 
-        if (viewModel.placeName.isEmpty()){
+        if (viewModel.placeName.isEmpty()) {
             viewModel.placeName = intent.getStringExtra("place_name") ?: ""
         }
 
-        viewModel.weatherLiveData.observe(this, Observer {
-            result ->
+        navBtn.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        drawerLayout.addDrawerListener(object : DrawerLayout.DrawerListener{
+            override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
+                Toast.makeText(SunnyWeatherApplication.context, "onDrawerSlide executed", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onDrawerOpened(drawerView: View) {
+                Toast.makeText(SunnyWeatherApplication.context, "onDrawerOpened executed", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onDrawerClosed(drawerView: View) {
+                //强制关闭软键盘输入法
+                val manager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                manager.hideSoftInputFromWindow(drawerView.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+            }
+
+            override fun onDrawerStateChanged(newState: Int) {
+                Toast.makeText(SunnyWeatherApplication.context, "onDrawerStateChanged executed", Toast.LENGTH_SHORT).show()
+            }
+
+        })
+
+        viewModel.weatherLiveData.observe(this, Observer { result ->
             val weather = result.getOrNull()
-            if (weather != null){
+            if (weather != null) {
                 showWeatherInfo(weather)
-            }else{
+            } else {
                 Toast.makeText(this, "无法成功获取天气信息", Toast.LENGTH_SHORT).show()
                 result.exceptionOrNull()?.printStackTrace()
             }
+            //结束刷新动画
+            swipeRefresh.isRefreshing = false
         })
 
-        viewModel.refreshWeather(viewModel.locationLng, viewModel.locationLat)
+        swipeRefresh.setColorSchemeResources(R.color.purple_200)
+        //首次进入页面 刷新天气
+        refreshWeather()
+        //添加刷新监听  手动刷新天气
+        swipeRefresh.setOnRefreshListener {
+            refreshWeather()
+        }
+
     }
 
-    private fun showWeatherInfo(weather: Weather){
+    fun refreshWeather(){
+        viewModel.refreshWeather(viewModel.locationLng, viewModel.locationLat)
+        swipeRefresh.isRefreshing = true
+    }
+
+    private fun showWeatherInfo(weather: Weather) {
         placeName.text = viewModel.placeName
         val realtime = weather.realtime
         val daily = weather.daily
@@ -76,10 +122,11 @@ class WeatherActivity : AppCompatActivity() {
         //渲染forecast
         forecastLayout.removeAllViews()
         val days = daily.skycon.size
-        for (i in 0 until days){
+        for (i in 0 until days) {
             val skycon = daily.skycon[i]
             val temperature = daily.temperature[i]
-            val view = LayoutInflater.from(this).inflate(R.layout.forecast_item, forecastLayout, false)
+            val view =
+                LayoutInflater.from(this).inflate(R.layout.forecast_item, forecastLayout, false)
             val dateInfo = view.findViewById<TextView>(R.id.dateInfo)
             val skyIcon = view.findViewById<ImageView>(R.id.skyIcon)
             val skyInfo = view.findViewById<TextView>(R.id.skyInfo)
